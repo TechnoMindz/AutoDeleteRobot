98339e4a366fecbfb893878edd4163703c632110
Ravinandan <104293257+RaviTechnoMindz@users.noreply.github.com>
2022-08-15 15:12:07 +0530

    fixes

--- plugins/banned.py
+++ plugins/banned.py
@@ -1,11 +1,10 @@
-# Banned Module
-# https://github.com/TechnoMindz/CommonBot_For_All
 from pyrogram import Client, filters
 from utils import temp
 from pyrogram.types import Message
 from database.users_chats_db import db
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from info import SUPPORT_CHAT
+
 async def banned_users(_, client, message: Message):
     return (
         message.from_user is not None or not message.sender_chat
@@ -22,7 +21,7 @@ disabled_group=filters.create(disabled_chat)
 @Client.on_message(filters.private & banned_user & filters.incoming)
 async def ban_reply(bot, message):
     ban = await db.get_ban_status(message.from_user.id)
-    await message.reply(f'Sorry Dude, You are Banned to use Me😤. \nBan Reason: {ban["ban_reason"]}')
+    await message.reply(f'Sorry Dude, You are Banned to use Me. \nBan Reason: {ban["ban_reason"]}')
 
 @Client.on_message(filters.group & disabled_group & filters.incoming)
 async def grp_bd(bot, message):
