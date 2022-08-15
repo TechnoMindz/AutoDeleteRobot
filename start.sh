242ec80658c3fa7cc8c97c555fc915305f83008d
Ravinandan <104293257+RaviTechnoMindz@users.noreply.github.com>
2022-08-15 15:22:12 +0530

    fixes

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TechnoMindz/AutoDeleteRobot.git /LUCIFER
+  git clone https://github.com/TechnoMindz/AutoDeleteRobot.git /AutoDeleteRobot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /AutoDeleteRobot
